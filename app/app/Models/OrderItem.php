aca4e702f34504228a6e732a08d0435379ffa150
root <root@Veniamin.>
2025-02-06 15:23:31 +0300

    blade added file, fix order bug1

--- app/app/Models/OrderItem.php
+++ app/app/Models/OrderItem.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
-
+    
     protected $fillable = ['order_id', 'product_id', 'quantity', 'price'];
 
     public function order()
